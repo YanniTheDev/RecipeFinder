c9c3d3ed3808646ccf59606ccfa04c12245ff3b9
YanniTheDev <yannithedev@gmail.com>
2024-04-22 21:10:39 +1200

    Added searchbar

---
+++ RecipeFinder/src/Components/Search.jsx
@@ -0,0 +1,10 @@
+import "../CSS Styles/Search.css";
+import "../CSS Styles/Reusables.css";
+
+export const Searchbar = () => {
+    return (
+        <div className="search-container flex-c-c">
+            <input type="text" className="search-box"/>
+        </div>
+    );
+}
