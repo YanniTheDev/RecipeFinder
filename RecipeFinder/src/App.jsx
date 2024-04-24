fd3c445b2fe6192695dca14738cf7fb39aa09a29
YanniTheDev <yannithedev@gmail.com>
2024-04-24 20:34:38 +1200

    Still working on API. Implemented loading spinner

--- RecipeFinder/src/App.jsx
+++ RecipeFinder/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
 import "./CSS Styles/App.css"
+import "./CSS Styles/Reusables.css";
+
+import { useState } from 'react'
 
 import Axios from "axios";
 
 import { Searchbar } from './Components/Search.jsx';
 
 function App() {
+  const [recipeList, setRecipeList] = useState([]);
 
   Axios.get("https://api.edamam.com/search?q=chicken&app_id=0e3ef802&app_key=a1b68ccf537c38d19a2af561d81b1bd9").then((response) => {
-    console.log(response.data.hits[0].recipe.calories);
+    console.log(response.data);
   });
 
   return (
-    <div>
+    <div className="flex-c-c">
       <Searchbar />
     </div>
   );
