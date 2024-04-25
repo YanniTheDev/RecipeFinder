8653c6f6cce96317a9bf22b7e0f8e3ff1933fbf5
YanniTheDev <yannithedev@gmail.com>
2024-04-25 20:50:48 +1200

    Made the recipe card

--- RecipeFinder/src/Components/Recipe.jsx
+++ RecipeFinder/src/Components/Recipe.jsx
@@ -1,8 +1,9 @@
 import "../CSS Styles/Reusables.css";
+import "../CSS Styles/Recipe.css";
 
 export const Recipe = (props) => {
     return (
-        <div>
+        <div className="individual-recipe-container">
             <h1>{props.recipeList.recipe.label}</h1>
             <p>Calories: {props.recipeList.recipe.calories}</p>
         </div>
